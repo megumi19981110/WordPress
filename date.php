a21291301649305e2a0aceaf8dfb79bdea8a4283
megumi19981110 <142647543+megumi19981110@users.noreply.github.com>
2024-03-12 19:38:50 +0900

    Add files via upload

--- date.php
+++ date.php
@@ -2,11 +2,9 @@
 
 <div id="container" class="wrapper">
   <main>
-    <?php
-      $cat = get_the_category();
-      $catname = $cat[0]->cat_name;
-    ?>
-    <h1 class="page-title"><?php echo $catname; ?>一覧</h1>
+    <h1 class="page-title">
+      <?php echo get_query_var('year').'年'.get_query_var('monthnum').'月'; ?>
+    </h1>
 
     <?php
       if (have_posts()):
--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-// Silence is golden.
+<?php get_header(); ?>
+
+<div id="container" class="wrapper">
+  <main>
+    <?php
+      if (have_posts()):
+        get_template_part('loop');
+
+        if (function_exists("pagination")) {
+          pagination($wp_query->max_num_pages);
+        }
+      endif;
+    ?>
+  </main>
+
+  <?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
+
+<?php get_footer(); ?>
